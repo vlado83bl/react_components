a2c8650cd21858def5ca07c55c90f308c8db60d0
Vladimir Vasilic <vlado83bl@gmail.com>
2024-03-25 12:26:58 +0100

    first commit for the reusable react components

---
+++ src/pages/ModalPage.js
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import Modal from '../components/Modal';
+import Button from '../components/Button';
+
+function ModalPage() {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClick = () => {
+    setShowModal(true);
+  };
+
+  const handleClose = () => {
+    setShowModal(false);
+  }
+
+  const actionBar = <Button onClick={handleClose} primary className='mt-8'>I accept!</Button>
+
+  const modal = <Modal onClose={handleClose} actionBar={actionBar}>
+      <p>
+        This is very important agreement to accept
+      </p>
+    </Modal>
+
+  return (
+    <div>
+      <Button onClick={handleClick} primary>
+        Open Modal
+      </Button>
+      {showModal && modal}
+    </div>
+  );
+}
+
+export default ModalPage;
